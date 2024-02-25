78951a7bd630c02916ddb20197cf9257c09a1c80
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-02-25 13:48:35 +0100

    Update LoginPage

--- medinsighthub-front/src/components/Login/Login.js
+++ medinsighthub-front/src/components/Login/Login.js
@@ -1,7 +1,21 @@
-import { Alert, TextField, Button, Container, Box, Typography } from '@mui/material';
 import { login } from '../../services/userService';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Avatar, Alert, TextField, Button, Container, Box, Typography, Link } from '@mui/material';
+import { LockOutlined } from '@mui/icons-material';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.primary" align="center" {...props}>
+    {'Copyright © '}
+    <Link color="inherit" href="https://mui.com/">
+      MedInsightHUB
+    </Link>{' '}
+    {new Date().getFullYear()}
+    {'.'}
+    </Typography>
+  )
+}
 
 const LoginForm = props => {
 	const [email, setEmail] = useState(null);
@@ -24,43 +38,60 @@ const LoginForm = props => {
 	};
 
 	return (
-		<Container component={'main'}>
-			<Box
-				sx={{
-					marginTop: 8,
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-				}}>
-				<Typography component='h1' variant='h5'>
-					Sign in
-				</Typography>
-				{errorMessage?.length > 0 ? (
-					<Alert style={{ width: '80%' }} severity='error' variant='filled'>
-						{errorMessage}
-					</Alert>
-				) : null}
-				<TextField
-					margin='normal'
-					name='email'
-					id='email'
-					placeholder='Enter your email...'
-					onChange={e => setEmail(e.target.value)}
-				/>
-				<TextField
+    <Container component={'main'} maxWidth='xs'>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+          <LockOutlined/>
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        {errorMessage?.length > 0 ? (
+          <Alert style={{ width: '80%' }} severity='error' variant='filled'>
+            {errorMessage}
+          </Alert>
+        ) : null}
+        <TextField
+          margin='normal'
+          label='Email Address'
+          name='email'
+          id='email'
+          placeholder='Enter your email...'
+          onChange={e => setEmail(e.target.value)}
+          autoComplete='email'
+          fullWidth
+          required
+        />
+        <TextField
+          margin='normal'
 					type='password'
-					margin='normal'
-					name='password'
-					id='password'
-					placeholder='Enter your password...'
-					onChange={e => setPassword(e.target.value)}
-				/>
-				<Button disabled={!email || !password} variant='contained' onClick={handleSubmit}>
-					Login
-				</Button>
-			</Box>
-		</Container>
-	);
-};
+          label='Password'
+          name='password'
+          id='password'
+          placeholder='Enter your password...'
+          onChange={e => setPassword(e.target.value)}
+          autoComplete='current-password'
+          fullWidth
+          required
+        />
+        <Button 
+          type='submit' 
+          fullWidth
+          disabled={!email || !password} 
+          variant='contained' 
+          onClick={handleSubmit}>
+          Login
+        </Button>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
+  );
+}
 
 export default LoginForm;
