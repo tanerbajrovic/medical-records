79d59c074f834355d32ed55b31ffbc166163e711
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-02-25 13:51:52 +0100

    Add NotFoundPage

--- medinsighthub-front/src/App.js
+++ medinsighthub-front/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginForm from './components/Login/Login';
 import HomePage from './components/HomePage/HomePage';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage'
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
@@ -11,13 +12,15 @@ function App() {
 				<Routes>
 					<Route
 						path='/'
+						exact
 						element={
 							<ProtectedRoute>
 								<HomePage />
 							</ProtectedRoute>
 						}
 					/>
-					<Route path='/login' element={localStorage.getItem('medhub-token') ? <Navigate to='/' /> : <LoginForm />} />
+					<Route path='/login' exact element={localStorage.getItem('medhub-token') ? <Navigate to='/' /> : <LoginForm />} />
+					<Route path='*' element={<NotFoundPage/>} />
 				</Routes>
 			</Router>
 		</div>
